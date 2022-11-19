d7028c7303b113c3a872f5e3478cbef3bf542219
palchikov <8antoxa8@gmail.com>
2022-11-19 13:46:21 +0200

    net_scanner

---
+++ net_scanner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+import scapy.all as scapy
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP / IP range')
+    return parser.parse_args()
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    client_list = []
+    for element in answered_list:
+        client_list.append({"ip": element[1].psrc, "mac-address": element[1].hwsrc})
+    return client_list
+
+
+def print_scan(results_list):
+    print("IP" + "\t\t\t" + "MAC-ADDRESS")
+    for element in results_list:
+        print(element["ip"] + "\t\t" + element["mac-address"])
+
+
+options = get_arguments()
+print_scan(scan(options.target))
+
